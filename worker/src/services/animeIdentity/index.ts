e5e981ff64639e01fda1d4fcd3b4bc54b62534e9
Grunet <sadikshahidain@gmail.com>
2024-12-06 04:08:02 +0000

    outline the needed logic

--- worker/src/services/animeIdentity/index.ts
+++ worker/src/services/animeIdentity/index.ts
@@ -7,6 +7,14 @@ function createAnimeIdentityService(): IAnimeIdentityService {
 		getAnimeInternalIdFromAnilistId({ anilistId }) {
 			// TODO - actually do the lookup or creation
 
+			// Check D1 to see if it's already there, return the internal id if so
+
+			// Hit the anilist API to see if the id is valid there
+			//   If it's not, return an error or throw an exception (maybe functional try catch) of some sort
+			//   If it is, create a new record in D1, and return the internal id you get
+
+			// Add overall exception handling
+
 			const animeInternalId = anilistId;
 
 			return {
