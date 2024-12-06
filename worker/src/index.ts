0206c7cdc170c1217fea61afff5a850f51de7ca9
Grunet <sadikshahidain@gmail.com>
2024-12-06 03:26:02 +0000

    Scaffold the routes for the app

--- worker/src/index.ts
+++ worker/src/index.ts
@@ -3,4 +3,24 @@ const app = new Hono();
 
 app.get('/', (c) => c.text('Hello Cloudflare Workers!'));
 
+app.get('/anime/random', (c) => {
+	c.status(501);
+	return c.text('Not Implemented');
+});
+
+app.get('/anime/:animeId', (c) => {
+	c.status(501);
+	return c.text('Not Implemented');
+});
+
+app.post('/user/anime/:animeId', (c) => {
+	c.status(501);
+	return c.text('Not Implemented');
+});
+
+app.get('/auth/anilist/redirect', (c) => {
+	c.status(501);
+	return c.text('Not Implemented');
+});
+
 export default app;
