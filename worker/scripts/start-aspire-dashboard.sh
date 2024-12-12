21ebe8e9bef8230730441eb671db1c6d8e6a5a99
Grunet <sadikshahidain@gmail.com>
2024-12-12 04:49:18 +0000

    Add helper methods for starting and stopping the aspire dashboard container

---
+++ worker/scripts/start-aspire-dashboard.sh
@@ -0,0 +1,5 @@
+docker run --rm -it \
+    -p 18888:18888 -p 4317:18889 \
+    -d --name aspire-dashboard \
+    -e DOTNET_DASHBOARD_UNSECURED_ALLOW_ANONYMOUS="true" \
+    mcr.microsoft.com/dotnet/aspire-dashboard:8.1.0
